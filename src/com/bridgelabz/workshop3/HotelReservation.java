d57d489e699de02afe2a5a5c136c2c48e29a1f12
VighneshBilgi <vhbilgi@gmail.com>
2022-09-02 12:47:40 +0530

    Added: UC1_Add_Hotel_Details

--- src/com/bridgelabz/workshop3/HotelReservation.java
+++ src/com/bridgelabz/workshop3/HotelReservation.java
@@ -9,5 +9,23 @@ public class HotelReservation {
     static Scanner sc = new Scanner(System.in);
 
     List<HotelPojo> hotelPojoList = new ArrayList<>();
+    public void addHotelDetails() {
 
+        HotelPojo hotelPojo = new HotelPojo();
+
+        System.out.println("Enter Hotel Name:");
+        String name = sc.next();
+        hotelPojo.setName(name);
+
+        System.out.println("Enter weekday rates for regular customer:");
+        double ratesWeekdayRegularCustomer = sc.nextDouble();
+        hotelPojo.setRateWeekdayRegularCustomer(ratesWeekdayRegularCustomer);
+
+        System.out.println("Enter weekend rates for regular customer:");
+        double ratesWeekendRegularCustomer = sc.nextDouble();
+        hotelPojo.setRateWeekendRegularCustomer(ratesWeekendRegularCustomer);
+
+        hotelPojoList.add(hotelPojo);
+
+    }
 }
--- src/com/bridgelabz/workshop3/HotelReservationMain.java
+++ src/com/bridgelabz/workshop3/HotelReservationMain.java
@@ -6,6 +6,40 @@ public class HotelReservationMain {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System :");
+        int choice;
 
+        HotelReservation hotelReservation = new HotelReservation();
+        do{
+
+            System.out.println("Select options provided in the menu below. Entering anything else will exit the menu :");
+            System.out.println("1. Add Hotel Details");
+            System.out.println("2. View All Hotel Details");
+            choice = sc.nextInt();
+
+            switch(choice){
+
+                case 1:
+                    hotelReservation.addHotelDetails();
+
+                    System.out.println("Printing All Hotel Details:");
+                    for (HotelPojo hotelElement: hotelReservation.hotelPojoList) {
+
+                        System.out.println(hotelElement);
+
+                    }
+                    break;
+                case 2:
+                    System.out.println("Printing All Hotel Details:");
+                    for (HotelPojo hotelElement: hotelReservation.hotelPojoList) {
+
+                        System.out.println(hotelElement);
+
+                    }
+                    break;
+                default:
+                    System.out.println("Exiting Menu...");
+            }
+
+        }while (choice == 1 || choice ==2);
     }
 }
